d3ac775beca0c62ab2c215ee311d24ac5583f71f
Tomasz Kula <tomaszkula07@gmail.com>
2016-07-07 11:43:34 +0200

    Change to arrow f

--- main.js
+++ main.js
@@ -2,43 +2,35 @@ var minesweeper = (function($) {
   const boardSize = 9;
   const bomb = '&#9760;';
   const flag = '&#8683;';
-  let board = fillBoard();
 
-  function styleContainerSize(){
+  let styleContainerSize = () => {
     let size = (boardSize * 26);
     $('#container').css( {
               'width' : size,
               'height' : size
     });
-  }
+  };
 
-  function appendContainer() {
+   let appendContainer = () => {
     $('body').append('<div id="container"></div>');
     styleContainerSize();
-  }
+  };
 
-  function appendGrid() {
+  let appendGrid = () => {
     let $container = $('#container');
     let gridSize = boardSize  * boardSize;
 
     for (var x = 1; x <= boardSize ; x++){
       for(var y = 1; y <= boardSize; y++) {
         let $grid =
-        $('<div class="grid" id="' + x + '_' + y + '"></div>');
+        $('<div class="grid" id="' + (x-1) + '_' + (y-1) + '"></div>');
         $container.append($grid);
       }
     }
 
-  }
+  };
 
-  function render() {
-    appendContainer();
-    appendGrid();
-    addBombs();
-    addHelperDigits();
-  }
-
-  function fillBoard() {
+  let fillBoard = () => {
     let board = [];
     for (let i = 0; i < boardSize; i++){
       let row = [];
@@ -48,14 +40,15 @@ var minesweeper = (function($) {
       board.push(row);
     }
     return board;
-  }
+  };
 
+  let board = fillBoard();
 
-  function getRandomCords() {
+  let getRandomCords = () => {
     return Math.floor(Math.random() * (boardSize-1) + 1);
-  }
+  };
 
-  function getBombCords(){
+  let getBombCords = () => {
     let x, y, emptyPosition;
     do {
       x = getRandomCords();
@@ -64,53 +57,56 @@ var minesweeper = (function($) {
     } while (emptyPosition !== '');
 
     return [x,y];
-  }
+  };
 
-  function addBomb(){
+  let addBomb = () => {
     let cords = getBombCords();
     let [x, y] = cords;
     board[x][y] = bomb;
-  }
+  };
 
-  function calculateNumOfBombs(){
+  let calculateNumOfBombs = () => {
     return Math.floor((boardSize * boardSize) * 10 / 100) + 1;
-  }
+  };
 
-  function addBombs() {
+  let addBombs = () => {
     let numOfBombs = calculateNumOfBombs();
     for (let i = 1; i <= numOfBombs; i++){
       addBomb();
     }
-  }
+  };
 
-  function isWithinAcceptableRange(element){
+  let isWithinAcceptableRange = (element) => {
     let x, y;
     [x, y] = element;
     return x >= 0 && y >= 0 && x <= boardSize - 1 && y <= boardSize - 1;
-  }
+  };
 
-  function selectAdjacentPositions(x, y){
+  let selectAdjacentPositions = (x, y) => {
     return [[x-1, y-1], [x-1, y],
         [x-1, y+1], [x, y+1],
           [x+1, y+1], [x+1, y],
-            [x+1, y-1], [x, y-1]].filter(isWithinAcceptableRange)
-  }
+            [x+1, y-1], [x, y-1]].filter(isWithinAcceptableRange);
+  };
 
-  function howManyAdjacentBombs(x, y){
+  let isOccupiedByBomb = (cords) => {
+    let x, y;
+    [x, y] = cords;
+    return board[x][y] == bomb;
+  };
+
+  let howManyAdjacentBombs = (x, y) => {
     adjacentPositions = selectAdjacentPositions(x,y);
 
     let count = 0;
-    adjacentPositions.forEach(function(cords){
-      let x, y;
-      [x, y] = cords;
-      if (board[x][y] == bomb) count++;
-    });
+    adjacentPositions
+    .forEach( cords => { if (isOccupiedByBomb(cords))  count++; } );
     return count;
-  }
+  };
 
-  function addHelperDigits(){
-    board.forEach(function(row, x){
-      row.forEach(function(value, y){
+  let addHelperDigits = () => {
+    board.forEach( (row, x) => {
+      row.forEach( (value, y) => {
         if (value == bomb) {
           return;
         } else {
@@ -119,34 +115,41 @@ var minesweeper = (function($) {
         }
       });
     });
-  }
+  };
 
-  function showContent() {
-    board.forEach(function(row, x){
-      row.forEach(function(value, y){
+  let showContent = () => {
+    board.forEach( (row, x) => {
+      row.forEach( (value, y) => {
         $('#' + x + '_' + y).html(value).text();
       });
     });
-  }
+  };
 
+  let render = () => {
+    appendContainer();
+    appendGrid();
+    addBombs();
+    addHelperDigits();
+  };
 
 
   return {
-    flag : function() { return flag; },
-    bomb : function() { return bomb; },
-    board : function() { return board; },
-    render : render,
-    showContent : showContent
+    flag        : () => { return flag; },
+    bomb        : () => { return bomb; },
+    board       : () => { return board; },
+    results     : () => { return results; },
+    render      : render,
+    showContent : showContent,
   };
 
 
 })(jQuery);
 
 
-$(document).ready(function(){
+$(document).ready( () => {
   minesweeper.render();
 
-  $('.grid').click(function(e){
+  $('.grid').click((e) => {
     $(this).toggleClass('active');
     console.log(event.target.id);
   });
